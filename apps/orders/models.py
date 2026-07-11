00e68ed2df2714d83af1ff4d76febba8651630bc
Himel Mazumder <himel.mazumder.dev@gmail.com>
2026-07-11 22:31:59 +0600

    create order related models

--- apps/orders/models.py
+++ apps/orders/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.conf import settings
+from apps.products.models import Product
 
 # Create your models here.
+
+class Order(models.Model):
+    STATUS_CHOICES = [
+        ('pending', 'Pending'),
+        ('paid', 'Paid'),
+        ('cancelled', 'Cancelled'),
+    ]
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='orders')
+    total_amount = models.DecimalField(max_digits=12, decimal_places=2, default=0.00)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='pending')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Order {self.id} = {self.user.username}"
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.PROTECT) # cannot delete a product if there is at least one order that has it
+    quantity = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    subtotal = models.DecimalField(max_digits=12, decimal_places=2)
+
+    # calculate the subtotal automatically when an order item is created or updated. doesn't work on bulk_create() or QuerySet.update() methods.
+    def save(self, *args, **kwargs):
+        self.subtotal = self.price * self.quantity
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.quantity} * {self.product.name} (Order {self.order.id})"
